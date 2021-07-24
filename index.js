683d4ba106c2d0610e3386b28d555b4ba0dda9d7
Biswarup Bhattacharjee <bbiswa471@gmail.com>
2021-07-24 21:48:37 +0530

    EDITED BY BISWA

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express=require('express');
+const expressJwt = require('express-jwt');
 const app = express();
 require('dotenv/config');
 bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const api=process.env.API_URL;
+const secret = process.env.secret;
 const cors = require('cors');
 const data=require('./data.json')
 const authJwt=require('./helpers/jwt');
@@ -14,7 +16,28 @@ app.options('*',cors());
 //Middlewears
 app.use(bodyParser.json())
 app.use(morgan('tiny'))
-app.use(authJwt)
+app.use(expressJwt({
+    secret,
+    algorithms: ['HS256'],
+    isRevoked: isRevoked
+}).unless({
+    path: [
+        {url: /\/public\/uploads(.*)/ , methods: ['GET', 'OPTIONS'] },
+        {url: /\/api\/v1\/products(.*)/ , methods: ['GET', 'OPTIONS'] },
+        {url: /\/api\/v1\/categories(.*)/ , methods: ['GET', 'OPTIONS'] },
+        {url: /\/api\/v1\/orders(.*)/,methods: ['GET', 'OPTIONS', 'POST']},
+        `${api}/users/login`,
+        `${api}/users/register`,
+    ]
+}))
+async function isRevoked(req, payload, done) {
+    if(!payload.isAdmin) {
+        done(null, true)
+    }
+
+    done();
+}
+
 app.use('/public/uploads',express.static(__dirname+'/public/uploads'))
 app.use(errorHandler);
 let port=process.env.PORT || 5000;
