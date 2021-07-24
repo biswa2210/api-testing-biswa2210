1990eab048d05e74c494d39686a32a33f52f573d
Biswarup Bhattacharjee <bbiswa471@gmail.com>
2021-07-24 21:12:56 +0530

    EDITED BY BISWA

--- index.js
+++ index.js
@@ -6,23 +6,20 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const api=process.env.API_URL;
 const cors = require('cors');
-const authJwt=require('./helpers/jwt');
-const errorHandler = require('./helpers/error-handler');
+const data=require('./data.json')
 app.use(cors());
 app.options('*',cors());
 //Middlewears
 app.use(bodyParser.json())
 app.use(morgan('tiny'))
-app.use(authJwt())
 app.use('/public/uploads',express.static(__dirname+'/public/uploads'))
-app.use(errorHandler);
 let port=process.env.PORT || 5000;
-/*app.get('/',(req,res)=>{
+app.get('/',(req,res)=>{
     res.send("Hello World"+api)
 })
 app.get('/developers',(req,res)=>{
     res.send(data)
-})*/
+})
 //Routers
 const productRouter=require('./routers/products');
 const userRouter=require('./routers/users');
