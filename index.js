2fd07e1edf454a543121ce378283429caa85d604
Biswarup Bhattacharjee <bbiswa471@gmail.com>
2021-07-24 17:53:41 +0530

    EDITED BY BISWA

--- index.js
+++ index.js
@@ -9,6 +9,18 @@ app.get('/',(req,res)=>{
 app.get('/developers',(req,res)=>{
     res.send(data)
 })
+//Database Connections---------------------------------------->
+mongoose.connect(process.env.CONNECTION_URL,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    dbName : 'daily-deals_database'
+})
+.then(()=>{
+    console.log("Your Database Connection Is Ready");
+}).catch((err)=>{
+    console.log(err);
+})
+//------------------------------------------------------------>
 app.listen(port,()=>{
     console.log("The Testing App is listening on the port of : "+port);
 })
